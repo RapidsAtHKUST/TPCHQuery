4327080a53ddaab3b23729d75c7d13a2dc080879
CHE Yulin <yche@cse.ust.hk>
2019-10-17 16:28:47 +0800

    add aio utils, remove makefile, add date utils

--- q3-src/tpch_q3_main.cpp
+++ q3-src/tpch_q3_main.cpp
@@ -2,9 +2,10 @@
 // Created by yche on 10/10/19.
 //
 
+#include <cassert>
+
 #include <chrono>
 #include <thread>
-#include <cassert>
 
 #include <omp.h>
 
@@ -19,6 +20,7 @@
 #include "util/primitives/boolarray.h"
 #include "util/primitives/blockingconcurrentqueue.h"
 #include "util/primitives/barrier.h"
+#include "util/aio.h"
 
 using namespace std;
 using namespace popl;
@@ -26,18 +28,32 @@ using namespace std::chrono;
 
 int main(int argc, char *argv[]) {
     OptionParser op("Allowed options");
-    auto string_option = op.add<Value<std::string>>("f", "file-path", "the graph bin file path");
+    auto customer_option = op.add<Value<std::string>>("c", "customer-path", "the customer file path");
+    auto order_option = op.add<Value<std::string>>("o", "order-path", "the order file path");
+    auto line_item_option = op.add<Value<std::string>>("l", "file-path", "the line item file path");
+
+    auto customer_filter_option = op.add<Value<std::string>>("0", "cf", "the custormer filter");
+    auto order_filter_option = op.add<Value<std::string>>("1", "of", "the order filter");
+    auto line_item_filter_option = op.add<Value<std::string>>("2", "lf", "the line item filter");
+    auto limit_option = op.add<Value<int>>("3", "limit", "the limit number");
     op.parse(argc, argv);
 
-    using Edge = pair<int32_t, int32_t>;
     Timer global_timer;
-    if (string_option->is_set()) {
-        log_info("OK");
-        sleep(1);
-        log_info("OK");
+    if (customer_option->is_set() && order_option->is_set() && line_item_option->is_set()) {
+        log_info("Path: %s, %s, %s", customer_option.get()->value().c_str(),
+                 order_option.get()->value().c_str(), line_item_option->value().c_str());
     }
-    thread t([]() {
-        log_info("Test");
-    });
-    t.join();
+    if (customer_filter_option->is_set() && order_filter_option->is_set() && line_item_filter_option->is_set()) {
+        log_info("Filter: %s, %s, %s", customer_filter_option.get()->value().c_str(),
+                 order_filter_option.get()->value().c_str(), line_item_filter_option.get()->value().c_str());
+        const char *date = order_filter_option.get()->value().c_str();
+        char date2[11];
+        date2[10] = '\0';
+        log_info("%d", ConvertDateToUint32(date));
+        ConvertUint32ToDate(date2, ConvertDateToUint32(date));
+        log_info("%s", date2);
+    }
+    if (limit_option->is_set()) {
+        log_info("Limit: %d", limit_option.get()->value());
+    }
 }
--- q3-src/util/util.h
+++ q3-src/util/util.h
@@ -18,12 +18,50 @@
 
 using namespace std;
 
+#define BASE_YEAR (1970)
+#define DATE_LEN (12)
+#define Y_MUL (10000)
+#define M_MUL (100)
+
+char table[64] = "";
+
+// Assume YYYY-MM-DD
+uint32_t ConvertDateToUint32(const char *date) {
+    char buf[11];
+    memcpy(buf, date, sizeof(char) * 11);
+    buf[4] = '\0';
+    buf[7] = '\0';
+    buf[10] = '\0';
+    return Y_MUL * atoi(buf) + M_MUL * atoi(buf + 5) + atoi(buf + 8);
+}
+
+// Asssume Large Enough for "YYYY-MM-DD" (10 chars)
+void ConvertUint32ToDate(char *date, uint32_t val) {
+    char buf[10];
+    stringstream ss;
+    ss << std::setw(4) << std::setfill('0') << val / Y_MUL << "-";
+    val %= Y_MUL;
+    ss << std::setw(2) << val / M_MUL << "-";
+    val %= M_MUL;
+    ss << std::setw(2) << val;
+    memcpy(date, ss.str().c_str(), 10);
+}
+
+
 template<class T>
 std::string FormatWithCommas(T value) {
     std::stringstream ss;
     ss.imbue(std::locale(""));
     ss << std::fixed << value;
     return ss.str();
+}
+
+inline void reset(std::stringstream &stream) {
+    const static std::stringstream initial;
+
+    stream.str(std::string());
+    stream.clear();
+    stream.copyfmt(initial);
 }
 
 inline size_t file_size(const char *file_name) {
