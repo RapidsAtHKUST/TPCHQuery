49e64bb03cc8e31e4f3836eca9f5c2a90bfb8a5e
CHE Yulin <yche@cse.ust.hk>
2019-10-18 17:03:37 +0800

    replace floating point checking 8s -> 3.5s

--- q3-src/file_parser.h
+++ q3-src/file_parser.h
@@ -10,10 +10,10 @@
 #include "util/log.h"
 #include "util/timer.h"
 
-#define IO_REQ_SIZE (128 * 1024)
+#define IO_REQ_SIZE (4 * 1024 * 1024)
 #define EXTRA_IO_SIZE (4 * 1024)
 
-#define NUM_IO_THREADS (16)
+#define NUM_IO_THREADS (40)
 #define NUM_PARSERS (16)
 #define NUM_BUFFERS (NUM_IO_THREADS +NUM_PARSERS)    // Must >= IO_THREADS
 
@@ -27,12 +27,12 @@ struct ParsingTask {
     ssize_t size_;
 };
 
-inline size_t FindStartIdx(char* buf){
+inline size_t FindStartIdx(char *buf) {
     size_t i = EXTRA_IO_SIZE;
     while (buf[i - 1] != LINUX_SPLITTER) {
         i--;
     }
-    return  i;
+    return i;
 }
 
 inline size_t LinearSearch(const char *str, size_t i, size_t len, char token) {
@@ -40,13 +40,133 @@ inline size_t LinearSearch(const char *str, size_t i, size_t len, char token) {
     return i;
 }
 
-inline int32_t atoi_range(const char *str, size_t beg, size_t end) {
-    assert(beg < end);
+inline int32_t StrToInt(const char *str, size_t beg, size_t end) {
     int sum = str[beg] - '0';
     for (auto i = beg + 1; i < end; i++) {
         sum = sum * 10 + (str[i] - '0');
     }
     return sum;
+}
+
+double latter_digits[] = {
+        1.0, 0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.0000001
+};
+
+inline double StrToFloat(const char *p, size_t beg, size_t end) {
+    double r = 0.0;
+    // Assume already 0-9 chars.
+    for (; beg < end && p[beg] != '.'; beg++) {
+        r = (r * 10.0) + (p[beg] - '0');
+    }
+    assert(p[beg] == '.');
+    beg++;
+
+    double frac = 0.0;
+    auto frac_size = end - beg;
+    for (; beg < end; beg++) {
+        frac = (frac * 10.0) + (p[beg] - '0');
+    }
+    r += frac * latter_digits[frac_size];
+    return r;
+}
+
+inline void ParseConsumer(ParsingTask task, char *strs, atomic_int &counter, mutex &mtx) {
+    auto buf = task.buf_;
+    auto i = FindStartIdx(buf);
+    while (i < task.size_) {
+        // 1st: CID.
+        size_t end = LinearSearch(buf, i, task.size_, COL_SPLITTER);
+        if (end == task.size_)return;
+
+        int32_t id = StrToInt(buf, i, end);
+        i = end + 1;
+
+        // 2nd: Parse Category
+        end = LinearSearch(buf, i, task.size_, LINUX_SPLITTER);
+        if (end == task.size_)return;
+
+        bool is_not_in = true;
+        int old_table_size = counter;
+        for (auto probe = 0; probe < old_table_size; probe++) {
+            // Probe.
+            if (memcmp(buf + i, strs + CUSTOMER_CATEGORY_LEN * probe, end - i) == 0) {
+                is_not_in = false;
+                break;
+            }
+        }
+        // If Fail.
+        if (is_not_in) {
+            // Mutex Lock.
+            unique_lock<mutex> lock(mtx);
+            int new_table_size = counter;
+            for (auto probe = old_table_size; probe < new_table_size; probe++) {
+                if (memcmp(buf + i, strs + CUSTOMER_CATEGORY_LEN * probe, end - i) == 0) {
+                    is_not_in = false;
+                    break;
+                }
+            }
+            if (is_not_in) {
+                memcpy(strs + new_table_size * CUSTOMER_CATEGORY_LEN, buf + i, end - i);
+                log_info("%.*s", end - i, strs + new_table_size * CUSTOMER_CATEGORY_LEN);
+                counter++;
+            }
+        }
+        i = end + 1;
+    }
+}
+
+inline void ParseOrder(ParsingTask task) {
+    auto buf = task.buf_;
+    auto i = FindStartIdx(buf);
+    while (i < task.size_) {
+        // 1st: OID.
+        size_t end = LinearSearch(buf, i, task.size_, COL_SPLITTER);
+        if (end == task.size_)return;
+        int32_t id = StrToInt(buf, i, end);
+        i = end + 1;
+        assert(id > 0);
+
+        // 2nd: CID.
+        end = LinearSearch(buf, i, task.size_, COL_SPLITTER);
+        if (end == task.size_)return;
+        int32_t cid = StrToInt(buf, i, end);
+        i = end + 1;
+        assert(cid > 0);
+
+        // 3rd: Order-Date.
+        end = LinearSearch(buf, i, task.size_, LINUX_SPLITTER);
+        if (end == task.size_)return;
+        uint32_t order_date = ConvertDateToUint32(buf + i);
+        i = end + 1;
+        assert(order_date > 0);
+    }
+}
+
+inline void ParseLineItem(ParsingTask task) {
+    auto buf = task.buf_;
+    auto i = FindStartIdx(buf);
+    while (i < task.size_) {
+        // 1st: OID.
+        size_t end = LinearSearch(buf, i, task.size_, COL_SPLITTER);
+        if (end == task.size_)return;
+        int32_t id = StrToInt(buf, i, end);
+        i = end + 1;
+        assert(id > 0);
+
+        // 2nd: Price.
+        end = LinearSearch(buf, i, task.size_, COL_SPLITTER);
+        if (end == task.size_)return;
+        double price = StrToFloat(buf, i, end);
+        i = end + 1;
+        assert(price > 0);
+
+        // 3rd: Ship-Date.
+        end = LinearSearch(buf, i, task.size_, LINUX_SPLITTER);
+        if (end == task.size_)return;
+        uint32_t ship_date = ConvertDateToUint32(buf + i);
+        i = end + 1;
+        assert(ship_date > 0);
+    }
 }
 
 template<typename F>
@@ -56,16 +176,16 @@ void ParseFileSeq(const char *file_name, F f) {
     auto size = file_size(file_name);
 
     char *buf = (char *) malloc(IO_REQ_SIZE * sizeof(char));
-    log_info("Start IO");
+    log_info("Start IO, Size: %zu", size);
     for (size_t i = 0; i < size; i += IO_REQ_SIZE - EXTRA_IO_SIZE) {
         ssize_t num_reads;
         if (i != 0) {
             num_reads = pread(file_fd, buf, IO_REQ_SIZE, i - EXTRA_IO_SIZE);
-            assert(num_reads <= IO_REQ_SIZE);
+//            assert(num_reads <= IO_REQ_SIZE);
         } else {
             num_reads = pread(file_fd, buf + EXTRA_IO_SIZE, IO_REQ_SIZE - EXTRA_IO_SIZE, i);
             buf[EXTRA_IO_SIZE - 1] = LINUX_SPLITTER;
-            assert(num_reads <= IO_REQ_SIZE - EXTRA_IO_SIZE);
+//            assert(num_reads <= IO_REQ_SIZE - EXTRA_IO_SIZE);
         }
         f({.buf_= buf, .size_= num_reads});
     }
@@ -104,7 +224,7 @@ void ParseFilePipeLine(const char *file_name, F f, int io_threads = NUM_IO_THREA
             }
         });
     }
-    log_info("Start IO");
+    log_info("Start IO, Size: %zu", size);
 #pragma omp parallel num_threads(io_threads)
     {
 #pragma omp for
@@ -115,14 +235,14 @@ void ParseFilePipeLine(const char *file_name, F f, int io_threads = NUM_IO_THREA
             if (tmp == nullptr) {
                 read_buffers.wait_dequeue(tmp);
             }
-            assert(tmp != nullptr);
+//            assert(tmp != nullptr);
             if (i != 0) {
                 num_reads = pread(file_fd, tmp, IO_REQ_SIZE, i - EXTRA_IO_SIZE);
-                assert(num_reads <= IO_REQ_SIZE);
+//                assert(num_reads <= IO_REQ_SIZE);
             } else {
                 tmp[EXTRA_IO_SIZE - 1] = LINUX_SPLITTER;
                 num_reads = pread(file_fd, tmp + EXTRA_IO_SIZE, IO_REQ_SIZE - EXTRA_IO_SIZE, i);
-                assert(num_reads <= IO_REQ_SIZE - EXTRA_IO_SIZE);
+//                assert(num_reads <= IO_REQ_SIZE - EXTRA_IO_SIZE);
             }
             parsing_tasks.enqueue({.buf_= tmp, .size_= num_reads});
         }
@@ -135,5 +255,40 @@ void ParseFilePipeLine(const char *file_name, F f, int io_threads = NUM_IO_THREA
         threads[i].join();
     }
     free(buf);
+    log_info("Read Time: %.6lfs, QPS: %.3lf GB/s", timer.elapsed(), size / timer.elapsed() / pow(10, 9));
+}
+
+/*
+ * F requires a single parameter (ParsingTask)
+ */
+template<typename F>
+void ParseFileSelf(const char *file_name, F f, int io_threads = NUM_IO_THREADS) {
+    Timer timer;
+    auto file_fd = open(file_name, O_RDONLY, S_IRUSR | S_IWUSR);
+    auto size = file_size(file_name);
+
+    log_info("Start IO, Size: %zu", size);
+#pragma omp parallel num_threads(io_threads)
+    {
+        char *tmp = (char *) malloc(IO_REQ_SIZE * sizeof(char));
+
+#pragma omp for
+        for (size_t i = 0; i < size; i += IO_REQ_SIZE - EXTRA_IO_SIZE) {
+            ssize_t num_reads;
+
+//            assert(tmp != nullptr);
+            if (i != 0) {
+                num_reads = pread(file_fd, tmp, IO_REQ_SIZE, i - EXTRA_IO_SIZE);
+//                assert(num_reads <= IO_REQ_SIZE);
+            } else {
+                tmp[EXTRA_IO_SIZE - 1] = LINUX_SPLITTER;
+                num_reads = pread(file_fd, tmp + EXTRA_IO_SIZE, IO_REQ_SIZE - EXTRA_IO_SIZE, i);
+//                assert(num_reads <= IO_REQ_SIZE - EXTRA_IO_SIZE);
+            }
+            f({.buf_= tmp, .size_= num_reads});
+        }
+        free(tmp);
+    }
+    log_info("Finish IO");
     log_info("Read Time: %.6lfs, QPS: %.3lf GB/s", timer.elapsed(), size / timer.elapsed() / pow(10, 9));
 }
--- q3-src/tpch_q3_main.cpp
+++ q3-src/tpch_q3_main.cpp
@@ -5,66 +5,15 @@
 #include <mutex>
 
 #include "util/program_options/popl.h"
-#include "util/primitives/parasort_cmp.h"
 #include "util/util.h"
-#include "util/pretty_print.h"
 #include "file_parser.h"
 
 using namespace std;
 using namespace popl;
 using namespace chrono;
 
-inline void ParseConsumer(ParsingTask task, char *strs, atomic_int &counter, mutex &mtx) {
-    auto buf = task.buf_;
-    auto i = FindStartIdx(buf);
-    auto prev_id = -1;
-    while (i < task.size_) {
-        // 1st: consumer ID.
-        size_t end = LinearSearch(buf, i, task.size_, COL_SPLITTER);
-        if (end == task.size_)return;
-
-        // Parse ID.
-        int32_t id = atoi_range(buf, i, end);
-        assert(id > prev_id);
-        prev_id = id;
-        i = end + 1;
-
-        // 2nd: char [10]
-        end = LinearSearch(buf, i, task.size_, LINUX_SPLITTER);
-        if (end == task.size_)return;
-
-        // Parse Category.
-        bool is_not_in = true;
-        int old_table_size = counter;
-        for (auto probe = 0; probe < old_table_size; probe++) {
-            // Probe.
-            if (memcmp(buf + i, strs + CUSTOMER_CATEGORY_LEN * probe, end - i) == 0) {
-                is_not_in = false;
-                break;
-            }
-        }
-        // If Fail.
-        if (is_not_in) {
-            // Mutex Lock.
-            unique_lock<mutex> lock(mtx);
-            int new_table_size = counter;
-            for (auto probe = old_table_size; probe < new_table_size; probe++) {
-                if (memcmp(buf + i, strs + CUSTOMER_CATEGORY_LEN * probe, end - i) == 0) {
-                    is_not_in = false;
-                    break;
-                }
-            }
-            if (is_not_in) {
-                memcpy(strs + new_table_size * CUSTOMER_CATEGORY_LEN, buf + i, end - i);
-                log_info("%.*s", end - i, strs + new_table_size * CUSTOMER_CATEGORY_LEN);
-                counter++;
-            }
-        }
-        i = end + 1;
-    }
-}
-
 int main(int argc, char *argv[]) {
+//    log_info("%.9lf", StrToFloat("1.3", 0, 3));
     OptionParser op("Allowed options");
     auto customer_option = op.add<Value<string>>("c", "customer-path", "the customer file path");
     auto order_option = op.add<Value<string>>("o", "order-path", "the order file path");
@@ -81,12 +30,20 @@ int main(int argc, char *argv[]) {
                  order_option.get()->value().c_str(), line_item_option->value().c_str());
         // IO-Buffer, IO-Size in the Cap (max: 128KB)
         auto customer_path = customer_option.get()->value().c_str();
-        atomic_int counter(0);
+        auto order_path = order_option.get()->value().c_str();
+        auto line_item_path = line_item_option.get()->value().c_str();
 
+        atomic_int counter(0);
         char *strs = (char *) malloc(1024 * CUSTOMER_CATEGORY_LEN * sizeof(char));
         mutex mtx;
-        ParseFilePipeLine(customer_path, [&counter, &strs, &mtx](ParsingTask task) {
+        ParseFileSelf(customer_path, [&counter, &strs, &mtx](ParsingTask task) {
             ParseConsumer(task, strs, counter, mtx);
+        });
+        ParseFileSelf(order_path, [](ParsingTask task) {
+            ParseOrder(task);
+        });
+        ParseFileSelf(line_item_path, [](ParsingTask task) {
+            ParseLineItem(task);
         });
     }
     if (customer_filter_option->is_set() && order_filter_option->is_set() && line_item_filter_option->is_set()) {
