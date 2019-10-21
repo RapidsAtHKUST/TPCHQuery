51f18e7cd74cda8b64ff7b442311ae4e861a4633
CHE Yulin <yche@cse.ust.hk>
2019-10-21 19:35:22 +0800

    update zip script

---
+++ zip_submission.py
@@ -0,0 +1,7 @@
+import datetime
+import os
+
+if __name__ == '__main__':
+    date_str = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")
+    print(date_str)
+    os.system('zip -r tpch-rapids-{}.zip . -x *cmake-build-debug* -x *.idea* -x *.py -x *.git* -x tpch-q3-submit'.format(date_str))
