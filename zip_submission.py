463fab73d30977b7df6ceeedaa897990992d739f
CHE Yulin <yche@cse.ust.hk>
2019-10-23 09:54:08 +0800

    exclude raw files

--- zip_submission.py
+++ zip_submission.py
@@ -4,4 +4,4 @@ import os
 if __name__ == '__main__':
     date_str = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")
     print(date_str)
-    os.system('zip -r tpch-rapids-{}.zip . -x *cmake-build-debug* -x *.idea* -x *.py -x *.git* -x tpch-q3-submit'.format(date_str))
+    os.system('zip -r tpch-rapids-{}.zip . -x /raw* -x *cmake-build-debug* -x *.idea* -x *.py -x *.git* -x tpch-q3-submit'.format(date_str))
